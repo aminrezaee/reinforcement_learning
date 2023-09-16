9d4e9e9bc0e972c26c63208db2bcd79dfa70dfd3
aminrezaee <rezaeea73@gmail.com>
2023-09-16 17:35:48 +0330

    debug

--- src/main.py
+++ src/main.py
@@ -12,10 +12,12 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-t' , '--timesteps' , default=1000 , type=int)
     args = parser.parse_args()
+    reward_sum = 0
+    reward = 0
     while environment.current_timestep < args.timesteps:
         is_done = False
-        # print("resetting")
-        first_position = environment.reset() # s_0
+        print(f"resetting:{reward_sum}")
+        first_position = environment.reset()
         agent.position = first_position
         # print(agent.position)
         environment.render(agent)
@@ -24,6 +26,8 @@ def main():
             new_position , reward , is_done , _ , _ = environment.step(agent , args.timesteps) # r_0
             print(environment.current_timestep)
             agent.step(reward , new_position , environment.current_timestep) # updates values and creates new action s_1 , a_1
+            if is_done:
+                reward_sum += reward 
             environment.render(agent)
     environment.create_video('world')
     return
